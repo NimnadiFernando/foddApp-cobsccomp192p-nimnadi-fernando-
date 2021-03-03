40858878ee5c81c5a09f82c467d81ced53bc67e1
NimnadiFernando <fernandonimnadi@gmail.com>
2021-03-03 14:20:39 +0530

    Initial Commit

---
+++ foddApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foddApp
+//
+//  Created by Admin on 2021-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
